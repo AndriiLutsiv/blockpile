39aad1090daaac07bbe337f2a638ec8da942687f
Andrii <66524005+AndriiLutsiv@users.noreply.github.com>
2023-11-30 10:25:52 +0200

    blog

---
+++ src/components/screens/blog/blog.jsx
@@ -0,0 +1,14 @@
+import styles from './blog.module.scss';
+
+
+const Blog = () => {
+
+    return <Layout title='Blog' description='Some description for seo'>
+        <div className={styles.blog}>
+
+        </div>
+    </Layout>
+
+}
+
+export default Blog;
---
+++ src/pages/blog.js
@@ -0,0 +1,8 @@
+import { Blog } from "@/components/screens/blog";
+
+export default function BlogPage({ }) {
+
+    return (
+        <Blog />
+    );
+}
