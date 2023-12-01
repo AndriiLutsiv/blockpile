4b84e2d43692350ff0b3ce0d567320021bdfd6bc
Andrii <66524005+AndriiLutsiv@users.noreply.github.com>
2023-12-01 18:04:33 +0200

    WP ACF, try catch, dynamic id post

--- src/components/ui/post/post.jsx
+++ src/components/ui/post/post.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import classNames from 'classnames';
 import styles from './post.module.scss';
+import Link from "next/link";
 
 const Post = ({ isSmall, post }) => {
+
   return <div className={styles.post}>
     <div className={classNames(styles.imgContainer, {[styles.smaller]: isSmall})}>
       <div className={styles.imageWrapper}>
@@ -15,7 +17,7 @@ const Post = ({ isSmall, post }) => {
       </div>
     </div>
     <div className={styles.category}>{post.categoryNames[0]}</div>
-    <h2 className={classNames(styles.title, {[styles.smaller]: isSmall})}>{post.title}</h2>
+    <Link href={`/posts/${post.id}`} className={classNames(styles.title, {[styles.smaller]: isSmall})}>{post.title}</Link>
     <p className={classNames(styles.text, {[styles.smaller]: isSmall})}>{post.excerpt}</p>
   </div>
 }
